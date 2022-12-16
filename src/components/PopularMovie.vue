bfd64380fc5f301ff55aaed470311a4c25b6502d
Sofia-Citrini <sofiacitrini@gmail.com>
2022-12-16 09:30:20 +0100

    aggiunto componente SingleCard a PopularMovie

--- src/components/PopularMovie.vue
+++ src/components/PopularMovie.vue
@@ -7,27 +7,7 @@
 
                 <div class="col-4 col-md-3 col-lg-2"
                     v-for="(popMovie, i) in store.popularMovies">
-                    <div class="my-card">
-                        <div class="img-container">
-                            <img v-if="popMovie.poster_path !== null"
-                                :src="`https://image.tmdb.org/t/p/w342${popMovie.poster_path}`" alt=""
-                                class="img-fluid">
-                            <img v-else
-                                src="https://images.pexels.com/photos/6373688/pexels-photo-6373688.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                                class="img-fluid">
-                        </div>
-                        <div class="my-card-body p-2">
-                            <div class="fw-bolder text-uppercase text-center py-2">{{ popMovie.title }}</div>
-                            <div class="py-2">
-                                <div class="text">Titolo originale </div>
-                                <div class="lh-1">{{ popMovie.original_title }}</div>
-                            </div>
-                            <div class="py-2" v-show="popMovie.overview !== ''">
-                                <div class="text">Overview</div>
-                                <p>{{ popMovie.overview }}</p>
-                            </div>
-                        </div>
-                    </div>
+                    <SingleCard :movie="popMovie"></SingleCard>
                 </div>
             </div>
         </div>
@@ -37,12 +17,15 @@
 
 <script>
 import { store } from "../store";
+import SingleCard from "./SingleCard.vue";
+
 export default {
     data() {
         return {
             store
-        }
-    }
+        };
+    },
+    components: { SingleCard }
 }
 
 </script>
